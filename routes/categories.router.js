80a4c5c1d1f355d2957c68242b50025d22ea8ef7
jdovale-tech <desarrollogrupo9@devitech.com.co>
2021-12-09 13:50:57 -0500

    Implementacion de capa de servicios(parcial)

--- routes/categories.router.js
+++ routes/categories.router.js
@@ -10,4 +10,30 @@ router.get('/:categoryId/products/:productId', (req, res) => {
   });
 });
 
+router.post('/', (req, res) => {
+  const body = req.body;
+  res.json({
+    message: "Created",
+    data: body,
+  });
+});
+
+router.patch('/:id', (req, res) => {
+  const { id } = req.params;
+  const body = req.body;
+  res.json({
+    message: "updated",
+    data: body,
+    id,
+  });
+});
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  res.json({
+    message: "deleted",
+    id,
+  });
+});
+
 module.exports = router;
--- routes/products.router.js
+++ routes/products.router.js
@@ -1,29 +1,19 @@
 const express = require('express');
-const faker = require('faker');
+const ProductsService = require('../services/product.service');
 
 const router = express.Router();
+const service = new ProductsService();
 
 router.get('/', (req, res) => {
-  const products = [];
-  const { size } = req.query;
-  const limit = size || 10;
-  for (let index = 0; index < limit; index++) {
-    products.push({
-      name: faker.commerce.productName(),
-      price: parseInt(faker.commerce.price(), 10),
-      image: faker.image.imageUrl(),
-    });
-  }
-  res.json(products);
+  const data = service.find();
+  res.status(200).json(data);
 });
 
 router.get('/:id', (req, res) => {
   const { id } = req.params;
-  res.json({
-    id,
-    name: 'producto_1',
-    price: 50000,
-  });
+  const product = service.findOne(id); 
+  res.status(200).json(product);
+ 
 });
 
 router.post('/', (req, res) => {
@@ -34,9 +24,34 @@ router.post('/', (req, res) => {
       statusCode: 404,
     });
   }
-  res.json({
+  res.status(201).json({
     message: 'Created',
     data: body,
+  });
+});
+
+router.patch('/:id', (req, res) => {
+  const { id } = req.params;
+  const body = req.body;
+  if(id === "999"){
+    res.status(304).json({
+      message: "Error, Not Modified ",
+    })
+  }else{
+    res.json({
+      message: 'updated',
+      data: body,
+      id
+    });
+  }
+  
+});
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  res.json({
+    message: 'deleted',
+    id
   });
 });
 
